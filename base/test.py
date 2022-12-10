58dfbd3e8e1015237b9522cc27a1c63055ec9b31
Leonardo Isso <leoisso.work@gmail.com>
2022-12-10 20:25:02 -0300

    feat: add Faker instance to test base

--- base/test.py
+++ base/test.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 
+from faker import Faker
 from mongoengine import connect, disconnect
 
 
 class WithMongoTestCase(TestCase):
+    faker = Faker()
+
     @classmethod
     def setUpClass(cls):
         connect("mongoenginetest", host="mongomock://localhost")
