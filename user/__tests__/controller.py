019e0f5721a81d314e3ef89997fbee13ef6a8c59
Leonardo Isso <leoisso.work@gmail.com>
2022-12-12 20:07:34 -0300

    fix: adjust unittest to the new implementation

--- user/__tests__/controller.py
+++ user/__tests__/controller.py
@@ -5,20 +5,26 @@ from base.test import WithMongoTestCase
 
 class UserControllerTest(WithMongoTestCase):
     def test_create_user(self):
-        alias = self.faker.name()
-        username = self.faker.user_name()
-        email = self.faker.email()
-        password = self.faker.password()
-
-        user_controller = UserController()
-        new_user = user_controller.create(
-            alias=alias,
-            username=username,
-            password=password,
-            email=email,
+        data = dict(
+            accounts=[
+                dict(
+                    alias=self.faker.name(),
+                    username=self.faker.user_name(),
+                )
+            ],
+            email=self.faker.email(),
+            password=self.faker.password(),
         )
 
-        self.assertEqual(new_user["email"], email)
-        self.assertEqual(new_user["password"], password)
-        self.assertEqual(new_user["accounts"][0]["alias"], alias)
-        self.assertEqual(new_user["accounts"][0]["username"], username)
+        user_controller = UserController()
+        new_user = user_controller.create(data)
+
+        self.assertEqual(new_user["email"], data["email"])
+        self.assertEqual(new_user["password"], data["password"])
+        self.assertEqual(
+            new_user["accounts"][0]["alias"], data["accounts"][0]["alias"]
+        )
+        self.assertEqual(
+            new_user["accounts"][0]["username"],
+            data["accounts"][0]["username"],
+        )
