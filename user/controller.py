bbe83a470bd2ec0792d848aa5550a246098b2d9d
Leonardo Isso <leoisso.work@gmail.com>
2022-12-14 19:15:30 -0300

    feat: add encryption to creation methods

--- user/controller.py
+++ user/controller.py
@@ -1,10 +1,20 @@
 import json
 from user.document import User
 
+from auth.utils import encrypt_password, generate_salt_key
+
 
 class UserController:
     def create(self, data):
-        user = User(**data)
+        user_data = data
+
+        salt = generate_salt_key()
+        user_data["salt"] = salt
+
+        encrypted_password = encrypt_password(data["password"], salt)
+        user_data["password"] = encrypted_password
+
+        user = User(**user_data)
         user.save()
 
         return json.loads(user.to_json())
