49377bf5fb91fd8005d1b6200f5ccadb17509f24
Leonardo Isso <leoisso.work@gmail.com>
2023-04-20 21:07:37 -0300

    feat: add proper encryption to passwords

--- auth/utils.py
+++ auth/utils.py
@@ -1,19 +1,12 @@
-import hashlib
-import string
-
-import secrets
+import bcrypt
 
 
 def generate_salt_key():
-    return "".join(
-        secrets.choice(string.ascii_uppercase + string.ascii_lowercase)
-        for _ in range(20)
-    )
+    return bcrypt.gensalt()
 
 
 def encrypt_password(password, salt):
-    salted_password = f"{password}{salt}"
-    return hashlib.md5(salted_password.encode()).hexdigest()
+    return bcrypt.hashpw(password.encode(), salt).decode()
 
 
 def validate_password(password, salt, user_password):
